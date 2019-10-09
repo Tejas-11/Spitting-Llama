7025582e0901071cde0a02699233b24d7e6ca7b7
Tejas-11 <tejastaneja11@gmail.com>
2019-10-09 23:34:35 +0530

    Create init.py
    initial commit of base character class

---
+++ character classes/init.py
@@ -0,0 +1,14 @@
+class Character:
+  width=0
+  reach=0
+  sense_range=0
+  health=0
+  position=(0,0)
+  weight=0
+  speed=0
+  damage=0
+  special_damage=0
+  level=0
+  cost=0
+  def sense():
+  def update():
